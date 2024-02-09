71fa13b46dd61d9d720108d37f000b9b43c067d8
doudoukiss <rayallen1996@gmail.com>
2024-02-09 12:33:59 +0900

    Initial Commit

---
+++ Thoughts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thoughts
+//
+//  Created by Seattle on 2024/2/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
